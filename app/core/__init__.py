a8336f9acd60c9782397d7b70ccf23c84a0f7515
Thomas Michalski <thomas_michalski@hotmail.com>
2025-04-09 10:13:10 -0400

    credentials off

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -15,7 +15,7 @@ def init_app(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.ALLOWED_ORIGINS,
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
